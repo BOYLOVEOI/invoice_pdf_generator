3501efb1ab1d54215b55ab0214c2b106eee4a891
Paul Lavoie <plavoie@gmu.edu>
2024-05-01 22:43:03 -0400

    Creating each invoice pdf for each excel file

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 # Import Statement
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 # Creating a list of the file paths for the multiple csv files to be processed
 # * is a wildcard that means to get everything that matches .xlsx in the
 # /invoices folder
 # If we put glob.glob('*') all the directories and files in the current folder
 # would be returned
-file_paths = glob.glob("invoices/*.xlsx")
+file_paths = glob.glob('invoices/*.xlsx')
+
 
 # for loop to iterate over and create dataframes for each spreadsheet
 for i in file_paths:
     # read_excel is to read in xlsx files
     df = pd.read_excel(i, sheet_name='Sheet 1')
+    # getting the file name ready dynamically through path.stem
+    filename = Path(i)
+    # getting the file str from filename.stem and splitting it and receiving
+    # only the first item, the invoice nr 
+    invoice_nr = filename.stem.split('-')[0]
+
+    # setting the format of the pdf document
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.set_font(family="Times", size=16, style="B")
+    # adding the first page
+    pdf.add_page()
+    # adding the title text cell
+    pdf.cell(w=50, h=8, txt="Invoice N.{}".format(invoice_nr))
+
+    # outputting each newly created pdf with their own names to the PDF folder
+    pdf.output("PDFS/{}.pdf".format(filename.stem))
+    
