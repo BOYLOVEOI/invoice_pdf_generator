f3b002b49303bc422c5cb7ae75cfe3fddb535557
Paul Lavoie <plavoie@gmu.edu>
2024-05-17 18:12:32 -0400

    Added the invoice date to the PDF files

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ for i in file_paths:
     df = pd.read_excel(i, sheet_name='Sheet 1')
     # getting the file name ready dynamically through path.stem
     filename = Path(i)
-    # getting the file str from filename.stem and splitting it and receiving
-    # only the first item, the invoice nr 
-    invoice_nr = filename.stem.split('-')[0]
+    # splitting the filename and unpacking the two elements, the invoice number 
+    # and date into two variables, invoice_nr and date 
+    invoice_nr, date = filename.stem.split('-')
 
     # setting the format of the pdf document
     pdf = FPDF(orientation="P", unit="mm", format="A4")
@@ -28,8 +28,12 @@ for i in file_paths:
     # adding the first page
     pdf.add_page()
     # adding the title text cell
-    pdf.cell(w=50, h=8, txt="Invoice N.{}".format(invoice_nr))
+    pdf.cell(w=50, h=8, txt="Invoice N.{}".format(invoice_nr), ln=1)
+    # adding the date below the title
+    pdf.cell(w=50, h=8, txt="Date: {}".format(date))
 
     # outputting each newly created pdf with their own names to the PDF folder
     pdf.output("PDFS/{}.pdf".format(filename.stem))
+
+
     
